9ce2fd2c97ff560eace336c018e6b7788623d956
Laura Wu <leemaiwu@gmail.com>
2023-05-31 12:19:47 -0600

    completed with math.reduce

--- solution.js
+++ solution.js
@@ -1,3 +1,13 @@
 const sumSquareEvenRootOdd = ns => {
+    for (let i = 0; i < ns.length; i++) {
+        if (ns[i] % 2 == 0) {
+            ns[i] = ns[i] * ns[i]
+        } else if (ns[i] % 2 !== 0) {
+            ns[i] = Math.sqrt(ns[i])
+        }
+    }
+    return Math.round(ns.reduce((a, b) => a + b, 0) * 100) / 100
+}
 
-};
+
+console.log(sumSquareEvenRootOdd([4,5,7,8,1,2,3,0]))
